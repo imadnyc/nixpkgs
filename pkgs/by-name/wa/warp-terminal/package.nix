c8903905da65b2ec90e7edd3caac167a82e37291
imad.nyc <me@imad.nyc>
2024-02-22 18:21:45 -0500

    warp-terminal: Add inital linux support

--- pkgs/by-name/wa/warp-terminal/package.nix
+++ pkgs/by-name/wa/warp-terminal/package.nix
@@ -2,11 +2,24 @@
 , stdenvNoCC
 , fetchurl
 , undmg
+, appimageTools
 }:
-stdenvNoCC.mkDerivation (finalAttrs: {
-  pname = "warp-terminal";
-  version = "0.2023.12.05.08.02.stable_00";
 
+let 
+pname = "warp-terminal";
+version = "0.2023.12.05.08.02.stable_00";
+
+linux = appimageTools.wrapType2 {
+  inherit pname version;
+  src = fetchurl {
+    url = "https://app.warp.dev/download?package=appimage";
+    hash = "sha256-541IHjrtUGzZwQh5+q4M27/UF2ZTqhznPX6ieh2nqUQ=";
+  };
+};
+
+
+darwin = stdenvNoCC.mkDerivation (finalAttrs: {
+  inherit pname version;
   src = fetchurl {
     url = "https://releases.warp.dev/stable/v${finalAttrs.version}/Warp.dmg";
     hash = "sha256-9olAmczIPRXV15NYCOYmwuEmJ7lMeaQRTTfukaYXMR0=";
@@ -24,13 +37,18 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
     runHook postInstall
   '';
+});
 
-  meta = with lib; {
-    description = "Rust-based terminal";
-    homepage = "https://www.warp.dev";
-    license = licenses.unfree;
-    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
-    maintainers = with maintainers; [ emilytrau Enzime ];
-    platforms = platforms.darwin;
-  };
-})
+meta = with lib; {
+  description = "Rust-based terminal";
+  homepage = "https://www.warp.dev";
+  license = licenses.unfree;
+  sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+  maintainers = with maintainers; [ emilytrau Enzime ];
+  platforms = platforms.darwin;
+};
+
+in 
+if stdenvNoCC.isDarwin
+then darwin
+else linux
